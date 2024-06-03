f832113808ad45e2ace87596d01db7ac07d305ac
Jakob Laid <jakob.laid@tptlive.ee>
2024-06-03 17:58:53 +0300

    first commit

---
+++ backend/App.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { auctionsController } from './controllers/auctions.js';
+import { signUpController } from './controllers/signup.js';
+import { loginController}  from './controllers/login.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/auctions', auctionsController);
+app.use('/api/login', loginController);
+app.use('/api/signup', signUpController);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
